44bb81daf426e930eb2abbe5ab7eb795b176763e
Sergej Pehterev <colorage503@gmail.com>
2019-04-24 12:53:41 +0300

    Create pavetra_station.ino

---
+++ pavetra_station.ino
@@ -0,0 +1,55 @@
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include "PMS.h"
+
+int deviceId = 601;
+int pm25;
+int pm10;
+String pm_data
+
+PMS pms(Serial);
+PMS::DATA data;
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(2, OUTPUT);
+
+  // === Turn ON LED ===
+  digitalWrite(2, LOW); 
+  delay(2000);
+
+  // === Connect to Internet ===
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Pavetra");
+
+  // === Get PM data ===
+  delay(10000);
+  if (pms.read(data)) {
+    pm25 = data.PM_AE_UG_2_5;
+    pm10 = data.PM_AE_UG_10_0;
+    pm_data = "{\"sensor_2_5\": " + pm25 + ", \"sensor_10\": " + pm10 + " }";
+  } else {
+    pm_data = "{\"sensor_2_5\": 0, \"sensor_10\": 0}";
+  }
+  
+  
+  // === HTTPS Request ===
+  HTTPClient http;
+  http.begin("http://pavetra.online/devices/data");
+  http.addHeader("Authorization", "Token ***");
+  int httpCode = http.POST(pm_data);
+
+  http.end();
+  
+  // === Sleep Mode ===
+  ESP.deepSleep(1*60*1000*1000); // Sleep 1 minute
+}
+
+void loop() {
+  
+}
