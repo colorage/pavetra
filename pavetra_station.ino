671eb0140f4c76e24a90ed31f64bf57dcdaba697
Sergej Pehterev <colorage503@gmail.com>
2019-04-24 15:01:00 +0300

    Update pavetra_station.ino

--- pavetra_station.ino
+++ pavetra_station.ino
@@ -5,7 +5,7 @@
 
 #include "PMS.h"
 
-int deviceId = 601;
+int deviceId = 601; // Specific ID
 int pm25;
 int pm10;
 String pm_data;
@@ -14,13 +14,14 @@ PMS pms(Serial);
 PMS::DATA data;
 
 void setup() {
-  Serial.begin(9600); 
-
+  Serial.begin(9600);
+  
   // === Connect to Internet ===
   WiFiManager wifiManager;
   wifiManager.autoConnect("pavetra");
 
   // === Get PM data ===
+  delay(30*1000);
   if (pms.readUntil(data)) {
     pm25 = data.PM_AE_UG_2_5;
     pm10 = data.PM_AE_UG_10_0;
@@ -28,17 +29,19 @@ void setup() {
   } else {
     pm_data = "{\"sensor_2_5\": 0, \"sensor_10\": 0}";
   }
+
+  Serial.println(pm_data);
   
   // === HTTPS Request ===
   HTTPClient http;
   http.begin("http://pavetra.online/devices/data");
-  http.addHeader("Authorization", "Token eyJhbGciOiJIUzI1NiJ9.eyJkZXZpY2VfaWQiOjQsImV4cCI6MTU4NDAyNDQzOH0.4GauS8uDUhX8LfGbGLL2VZ9ZeO6j7C4uXAj2RoLx3XU");
+  http.addHeader("Authorization", "Token ***");
   int httpCode = http.POST(pm_data);
-
   http.end();
 
+  
   // === Sleep Mode ===
-  ESP.deepSleep(1*60*1000*1000); // Sleep 1 minute
+  ESP.deepSleep(20*60*1000*1000); // Sleep 20 minutes
 }
 
 void loop() {
