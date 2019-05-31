8effd143af0168aadaae21a5e22dca61b1e8a4f1
turok <tohaturok@gmail.com>
2019-05-31 16:52:33 +0300

    refactroing firmware

--- pavetra_station.ino
+++ pavetra_station.ino
@@ -14,28 +14,39 @@ PMS::DATA data;
 
 void setup() {
   Serial.begin(9600);
-  
   // === Connect to Internet ===
   WiFiManager wifiManager;
   wifiManager.autoConnect("pavetra");  
-
   // === Get PM data ===
   pms.wakeUp();
-  delay(30*1000);
-  int i = 0;
-  while (i < 10) {
+}
+
+void loop() {
+  delay(30*1000);  
+  readDataFromPMS();
+  sendDataToServer();
+  // === Sleep Mode ===
+  pms.sleep();
+  ESP.deepSleep(20*60*1000*1000); // Sleep 20 minutes  
+}
+
+void readDataFromPMS() {
+  int count_read = 0;
+  int count_try_to_read = 0;
+  while (count_read < 10 && count_try_to_read < 10) {
     if (pms.readUntil(data)) {
       pm25 += data.PM_AE_UG_2_5;
       pm10 += data.PM_AE_UG_10_0;
-      i++;
-      delay(1000);
+      ++count_read;
     } else {
-      pm_data = "{\"sensor_2_5\": 0, \"sensor_10\": 0}";
+      ++count_try_to_read;
     }
   }
   pm_data = "{\"sensor_2_5\": " + String(pm25 / 10.0) + ", \"sensor_10\": " + String(pm10 / 10.0) + " }";
   Serial.println(pm_data);
-  
+}
+
+void sendDataToServer() {
   // === HTTPS Request ===
   HTTPClient http;
   http.begin("https://pavetra.online/devices/data", "F5 1B 7D A0 A5 8E 9E 07 9E 9F 7E F9 0E 0E 8A BE 57 D1 9A 5A");
@@ -43,13 +54,4 @@ void setup() {
   http.addHeader("Content-Type", "application/json");
   int httpCode = http.POST(pm_data);
   http.end();
-
-  
-  // === Sleep Mode ===
-  pms.sleep();
-  ESP.deepSleep(20*60*1000*1000); // Sleep 20 minutes
-}
-
-void loop() {
-  
 }
